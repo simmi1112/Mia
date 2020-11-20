3853727eba8b10ec33566f7d446ad7cf68fd1260
simmi <simmi@simmi.org>
2020-11-20 06:06:12 -0500

    Initial commit

---
+++ Assets/Macros/SpineAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineAnimation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+
+        Vector3 screenPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        RaycastHit2D hit = Physics2D.Raycast(screenPos, Vector2.zero);
+
+        if (hit)
+        {
+
+        }
+    }
+}
